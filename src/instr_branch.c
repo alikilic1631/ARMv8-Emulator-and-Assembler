f8059f36723c1e69ed65d0bd9c0aeef7218ecf3f
Ali Kilic <ak5323@texel02.doc.ic.ac.uk>
2024-06-03 18:00:03 +0100

    branch draft

--- src/instr_branch.c
+++ src/instr_branch.c
@@ -1,6 +1,65 @@
 #include "instr_branch.h"
 
+#define UncondTest 0x7E000000
+#define UncondExpected 0xA000000
+
+#define RegisterTest 0xFFFFFC1F
+#define RegisterExpected 0xD61F0000
+
+#define CondTest 0b11111111 << 23
+#define CondExpected 0b01010100 << 23
+
+#define EQ 0
+#define NE 1
+#define GE 10
+#define LT 11
+#define GT 12
+#define LE 13
+#define AL 14
+
 bool exec_branch_instr(emulstate *state, ulong raw)
 {
-  return false;
+  if ((raw & UncondTest) == UncondExpected){
+    ulong simm26 = get_value(raw,0,26);
+    ullong offset = simm26*4;
+    state->pc += sign_extend(offset, get_value(simm26, 0, 26));
+  }
+
+  else if ((raw & RegisterTest) == RegisterExpected){ 
+    ulong xn = get_value(raw,5,5);
+    state->pc += get_reg(state, 1, xn);
+  }
+
+  else if ((raw & CondTest) == CondExpected) {
+    ulong simm19 = get_value(raw,5,19);
+    ullong offset = simm19*4;
+    ulong cond = get_value(raw,0,4);
+    bool execute = 0;
+
+    switch (cond){
+      case EQ:
+        execute = (state->pstate.zero == 1);
+      case NE:
+        execute = (state->pstate.zero == 0);
+      case LT:
+        execute = (state->pstate.negative == state->pstate.overflow);
+      case GT:
+        execute = (state->pstate.overflow != state->pstate.overflow);
+      case LE:
+        execute = !((state->pstate.zero == 0) & (state->pstate.negative == state->pstate.overflow));
+      case AL:
+        execute = 1;
+
+      if (execute) {
+        state->pc += sign_extend(simm19, get_value(simm19, 0, 19));
+      } 
+    }
+
+  }
+
+  else {
+    return 0;
+  }
+
+  return 1;
 }
