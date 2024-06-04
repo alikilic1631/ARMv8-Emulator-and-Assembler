86fadde7e85735e1bba54dc0ccb9403eacc08a64
Eren Geridonmez <eg1623@ic.ac.uk>
2024-06-04 22:40:42 +0100

    added explicit declaration for set_pstate_flags

--- src/instr_dpimm.h
+++ src/instr_dpimm.h
@@ -2,4 +2,5 @@
 
 typedef unsigned long ulong;
 
+extern void set_pstate_flags(emulstate *state, bool sf, ullong result, ullong rn, ullong op2, bool add);
 extern bool exec_dpimm_instr(emulstate *state, ulong raw);
