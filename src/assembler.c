956e78ebdf0995b065edfd68e563e1bb1c36add5
Gleb Koval <gleb@koval.net>
2024-06-12 22:06:26 +0100

    Move instruction_type to .c file
    Co-Authored-By: Eren Geridonmez <eg1623@ic.ac.uk>

--- src/assembler.c
+++ src/assembler.c
@@ -3,6 +3,24 @@
 #include <string.h>
 #include "assembler.h"
 
+char *data_processing[] = {"add", "adds", "sub", "subs", "cmp", "cmn", "neg", "negs",
+                           "and", "ands", "bic", "bics", "eor", "orr", "eon", "orn", "tst", "movk", "movn",
+                           "movz", "mov", "mvn", "madd", "msub", "mul", "mneg", NULL};
+char *branching[] = {"b", "b.cond", "br", NULL};
+char *loads_stores[] = {"str", "ldr", NULL};
+
+bool instruction_type(const char *instr, char **array)
+{
+  for (int i = 0; array[i] != NULL; i++)
+  {
+    if (strcmp(instr, array[i]) == 0)
+    {
+      return true;
+    }
+  }
+  return false;
+}
+
 void add_symbol(const char *label, int address)
 {
   if (symbol_count >= MAX_SYMBOLS)
--- src/assembler.h
+++ src/assembler.h
@@ -14,20 +14,5 @@ typedef struct
 symbol_t symbol_table[MAX_SYMBOLS];
 int symbol_count = 0;
 
-char *data_processing[] = {"add", "adds", "sub", "subs", "cmp", "cmn", "neg", "negs",
-                           "and", "ands", "bic", "bics", "eor", "orr", "eon", "orn", "tst", "movk", "movn",
-                           "movz", "mov", "mvn", "madd", "msub", "mul", "mneg", NULL};
-char *branching[] = {"b", "b.cond", "br", NULL};
-char *loads_stores[] = {"str", "ldr", NULL};
-
-bool instruction_type(const char *instr, char **array)
-{
-    for (int i = 0; array[i] != NULL; i++)
-    {
-        if (strcmp(instr, array[i]) == 0)
-        {
-            return true;
-        }
-    }
-    return false;
-}
+extern void first_pass(FILE *fin);
+extern void second_pass(FILE *fin, FILE *fout);
