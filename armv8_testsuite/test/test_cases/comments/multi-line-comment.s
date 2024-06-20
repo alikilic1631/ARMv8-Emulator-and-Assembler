1f49b936c48a933d2d5bcb3e1cd8a42df99348cd
Gleb Koval <gleb@koval.net>
2024-06-20 16:09:11 +0100

    More advanced comments test cases

--- armv8_testsuite/test/test_cases/comments/multi-line-comment.s
+++ armv8_testsuite/test/test_cases/comments/multi-line-comment.s
@@ -1,7 +1,6 @@
 sub x7, x7, #0xa5c, lsl #0
-sub x15, x15, #0xa63, lsl #0
-ands w21, w7, w15
+sub x15, x15, #0xa63, /* comment */ lsl #0
+/* another comment */ ands w21, w7, w15
 and x0, x0, x0
 /* this multi-line comment
-will be ignored
-*/
+will be ignored */
